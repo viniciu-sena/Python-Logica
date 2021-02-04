d039b4ca77dbef33cc39443bd67daa1b4061fddf
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-04 11:05:48 -0300

    Create L1E08.py

---
+++ lista01/L1E08.py
@@ -0,0 +1,7 @@
+#08
+valorh = float(input("Digite o valor/hora: "))
+horas = int(input("Digite o numero de horas trabalhadas (no mês): "))
+
+salario = valorh * horas
+
+print("Seu salário é R$", salario)
