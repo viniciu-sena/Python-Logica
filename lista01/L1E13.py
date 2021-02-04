43f8addf231842eaa74ec779968dbb7b36dcd215
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-04 11:07:36 -0300

    Create L1E13.py

---
+++ lista01/L1E13.py
@@ -0,0 +1,10 @@
+#13
+sex = input("Digite M para identificar seu sexo como masculino ou F para feminino:")
+h = float( input("Digite sua altura: ") )
+
+if sex.lower() == "m":
+  h = (72.7*h) - 58
+  print("%.2f" % h, "kg é seu peso ideal" )
+elif sex.lower() == "f":
+  h = (62.1*h) - 44.7
+  print("%.2f" % h, "kg é seu peso ideal" )
