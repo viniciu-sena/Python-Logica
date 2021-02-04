5ab71d5eb66d1c73092de967d95d444ae2a0e9f0
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-04 11:06:58 -0300

    Create L1E11.py

---
+++ lista01/L1E11.py
@@ -0,0 +1,8 @@
+#11
+int01 = int(input("informe um valor inteiro: "))
+int02 = int(input("informe um valor inteiro: "))
+real01 = float(input("informe um valor real: "))
+
+print("A)= ", ((int01*2)*(int02/2)))
+print("B)= ", ((int01*3)+(real01)))
+print("C)= ", pow(real01, 3))
