ec9d106f732d13c0e6d3f471c9234dc6c8a3544d
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-05 11:21:43 -0300

    Create L2E10.py

---
+++ lista02/L2E10.py
@@ -0,0 +1,16 @@
+#10
+msg = ".."
+
+print("Turnos: M-matutino, V-vespertino e N-noturno")
+turno = input("Informe o turno em que voce estuda: ")
+
+if turno in "mM":
+  msg = "Bom dia"
+elif turno in "vV":
+  msg = "Boa tarde"
+elif turno in "nN":
+  msg = "Boa noite"
+else:
+  msg = "Valor invalido"
+
+print(msg)
