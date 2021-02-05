94380c2ecb7eafbae182f00ef11e4883b73f9bcb
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-05 09:05:25 -0300

    Add files via upload

---
+++ lista02/L02E03.py
@@ -0,0 +1,8 @@
+print("F - Feminino | M - Masculino")
+sex = input("Informe o seu sexo:")
+
+if sex.lower() == "f": print("Sexo Feminino")
+
+elif sex.lower() == "m": print("Sexo Masculino")
+
+else: print("Sexo Invalido")
