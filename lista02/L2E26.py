a98303f8aa79b2f111dd36736293aa366c353fbc
Kelvin Schneider <54813386+KelvinSchneider@users.noreply.github.com>
2021-02-05 14:22:42 -0300

    Create L2E26.py

---
+++ lista02/L2E26.py
@@ -0,0 +1,33 @@
+#26
+
+print("Digite [A] - Alcool")
+print("Digite [G] - Gasolina")
+tipo = input("Digite o tipo de combustivel: ")
+litros = float(input("Digite a quantidade de litros vendidos: "))
+
+precoA = 1.90
+precoG = 2.50
+
+if (tipo.lower() == "a"):
+  if (litros <= 20):
+    desconto = (precoA * 1.03) - precoA 
+    precoA = precoA - desconto
+    preco = litros * precoA
+  
+  elif (litros > 20):
+    desconto = (precoA * 1.05) - precoA 
+    precoA = precoA - desconto
+    preco = litros * precoA
+
+elif (tipo.lower() == "g"):
+  if (litros <= 20):
+    desconto = (precoG * 1.04) - precoG 
+    precoG = precoG - desconto
+    preco = litros * precoG
+  
+  elif (litros > 20):
+    desconto = (precoG * 1.06) - precoG 
+    precoG = precoG - desconto
+    preco = litros * precoG
+
+print("O total a ser pago é: R$ %.2f" % preco)
