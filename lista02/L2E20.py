49e2e9ececa7d6d81333b5445707c4e8cafdec71
Kelvin Schneider <54813386+KelvinSchneider@users.noreply.github.com>
2021-02-05 14:21:58 -0300

    Create L2E20.py

---
+++ lista02/L2E20.py
@@ -0,0 +1,17 @@
+#20
+
+nota1 = float(input("Digite a primeira nota: "))
+nota2 = float(input("Digite a segunda nota: "))
+nota3 = float(input("Digite a terceira nota: "))
+
+media = (nota1 + nota2 + nota3) / 3
+print("Média: %.2f" % media)
+
+if (media >= 7):
+  if (media == 10):
+    print("Aprovado com Distinção!")
+  else:
+    print("Aprovado!")
+
+elif (media < 7):
+  print("Reprovado!")
