4efa940c25885d96ecc659c4d383a6a6b0c44f77
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-05 11:23:58 -0300

    Create L2E25.py

---
+++ lista02/L2E25.py
@@ -0,0 +1,20 @@
+#25
+relatorio = []
+
+print("Responda com s/n")
+relatorio.append(input("Telefonou para a vitima? "))
+relatorio.append(input("Esteve no local do crime? "))
+relatorio.append(input("Mora perto da vítima? "))
+relatorio.append(input("Devia para a vítima? "))
+relatorio.append(input("Já trabalhou com a vítima? ")) 
+
+val = relatorio.count("s")
+
+if val == 2:
+  print("Suspeito")
+elif val > 2 and val <= 4:
+  print("Cumplice")
+elif val == 5:
+  print("Assassino")
+else:
+  print("Inocente")
