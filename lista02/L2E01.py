0f69212b85b18b9d2c127d2592c8199f25f6fcec
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-05 11:20:00 -0300

    Create L2E01.py

---
+++ lista02/L2E01.py
@@ -0,0 +1,10 @@
+#01
+val01 = int(input("Informe o 1° valor: "))
+val02 = int(input("Informe o 2° valor: "))
+
+if val01 > val02:
+  print("O maior valor é: ", val01)
+elif val01 < val02:
+  print("O maior valor é: ", val02)
+else:
+  print("Os dois são iguais")
