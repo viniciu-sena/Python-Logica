cabd906c20546995bcf0f891fc4f2738a4e19ded
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-05 09:59:24 -0300

    Add files via upload

---
+++ lista02/L02E09.py
@@ -0,0 +1,6 @@
+num1 = float( input("Digite o 1° valor: "))
+num2 = float( input("Digite o 2° valor: "))
+num3 = float( input("Digite o 3° valor: "))
+numbers = [num1,num2,num3]
+
+print(sorted(numbers))
