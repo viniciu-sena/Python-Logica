9eb96af71250b423533e7d3ebefa169e28f4c488
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-05 11:22:04 -0300

    Create L2E13.py

---
+++ lista02/L2E13.py
@@ -0,0 +1,12 @@
+#13
+import numpy as np
+semana = ["domingo","segunda","terca","quarta","quinta","sexta","sabado"]
+dia = int(input("informe o dia da semana: "))
+
+try:
+  if dia <= 0:
+    print("Valor invalido")
+  else:
+    print(semana[dia-1])
+except:
+  print("Valor invalido")
