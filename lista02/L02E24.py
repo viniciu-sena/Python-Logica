30e01d28eaf9c53f84cd653e1c4c0d64c761cc2f
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-05 14:03:19 -0300

    Add files via upload

---
+++ lista02/L02E24.py
@@ -0,0 +1,69 @@
+num1 = float( input("Digite o valor 1: "))
+num2 = float( input("Digite o valor 2: "))
+result = 0
+print("1 para multiplicacao")
+print("2 para divisao")
+print("3 para adicao")
+print("4 para subtracao")
+inp = int(input(">>"))
+
+if inp == 1:
+  result = num1 * num2
+  if result % 2 == 0:
+    print("Par")
+  else: print("Impar")
+  if result > 0:
+    print("Positivo")
+  else:print("Negativo")
+  if int(result) == result:
+    print("Inteiro")
+  else: print("Decimal")
+  print(num1 , " * " , num2 , " = " , result)
+
+elif inp == 2:
+    result = num1 / num2
+    if result % 2 == 0:
+        print("Par")
+    else:
+        print("Impar")
+    if result > 0:
+        print("Positivo")
+    else:
+        print("Negativo")
+    if int(result) == result:
+        print("Inteiro")
+    else:
+        print("Decimal")
+    print(num1, " / ", num2, " = ", result)
+
+elif inp == 3:
+    result = num1 + num2
+    if result % 2 == 0:
+        print("Par")
+    else:
+        print("Impar")
+    if result > 0:
+        print("Positivo")
+    else:
+        print("Negativo")
+    if int(result) == result:
+        print("Inteiro")
+    else:
+        print("Decimal")
+    print(num1, " + ", num2, " = ", result)
+
+elif inp == 4:
+    result = num1 - num2
+    if result % 2 == 0:
+        print("Par")
+    else:
+        print("Impar")
+    if result > 0:
+        print("Positivo")
+    else:
+        print("Negativo")
+    if int(result) == result:
+        print("Inteiro")
+    else:
+        print("Decimal")
+    print(num1, " - ", num2, " = ", result)
