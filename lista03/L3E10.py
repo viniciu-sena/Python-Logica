719e1a589e2531b340c11bdb4d2f9db4af2c9be1
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-08 16:36:24 -0300

    Add files via upload

---
+++ lista03/L3E10.py
@@ -0,0 +1,5 @@
+num1 = int(input("Informe um número inteiro: "))
+num2 = int(input("Informe um número inteiro maior que o anterior: "))
+
+for x in range(num1+1,num2):
+    print(x)
---
+++ lista03/L3E12.py
@@ -0,0 +1,5 @@
+num1 = int(input("De qual número deseja ver a tabuada:  "))
+
+print("Tabuado do" , num1 , ":")
+for i in range(1,11):
+    print(num1 , " x " , i , " = " , num1 * i)
