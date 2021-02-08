7def566e1ac5da0054a0a835cf00ea18ea838703
Vinícius Silva Sena <54813409+viniciu-sena@users.noreply.github.com>
2021-02-08 14:16:33 -0300

    Create L3E18.py

---
+++ lista03/L3E18.py
@@ -0,0 +1,25 @@
+#18
+n = int(input("N° de valores: "))
+
+i = 0
+valores = []
+
+while i < n:
+    valores.append(int(input("informe o valor: ")))
+    i += 1
+
+maior = 0
+soma = 0
+
+for i in range(len(valores)):
+  soma += valores[i]
+  if valores[i] >= maior:
+    maior = valores[i]
+
+menor = maior
+
+for i in range(len(valores)):
+  if valores[i] <= menor:
+    menor = valores[i]
+
+print("Maior: ",maior," | Menor: ",menor," | Soma: ",soma)
