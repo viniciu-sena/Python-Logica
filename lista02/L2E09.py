039aea2b68f14696cbf9aa48d310c27a137eaa24
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-09 10:28:06 -0300

    Update L2E09.py

--- lista02/L2E09.py
+++ lista02/L2E09.py
@@ -1,3 +1,6 @@
+# Desenvolvido por Daniel Schinaider de Oliveira
+# Exercício número 9
+
 num1 = float( input("Digite o 1° valor: "))
 num2 = float( input("Digite o 2° valor: "))
 num3 = float( input("Digite o 3° valor: "))
