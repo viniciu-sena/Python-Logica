9c1d0c8b5fe4ce9da9f220423da434a47f2913ba
Kelvin Schneider <54813386+KelvinSchneider@users.noreply.github.com>
2021-02-09 10:18:20 -0300

    Create L4E08.py

---
+++ lista04/L4E08.py
@@ -0,0 +1,24 @@
+#08
+
+idade = []
+altura = []
+
+i = 0
+while (i < 5):
+  tec = int(input("Digite sua idade: "))
+  idade.append(tec)
+
+  tec = float(input("Digite sua altura [1.70]: "))
+  altura.append(tec)
+
+  i+=1
+
+print("\n------------ idade -------------- \n")
+
+for i in reversed(idade):
+  print(i)
+
+print("\n------------ altura ------------- \n")
+
+for i in reversed(altura):
+  print(i)
