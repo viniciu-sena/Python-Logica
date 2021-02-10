c00771b7a8048efe2033b696b45428f748558a64
Daniel Schinaider <54813427+DanielSchinaider@users.noreply.github.com>
2021-02-10 16:39:06 -0300

    Delete L6E03.py

--- L6E03.py
+++
@@ -1,9 +0,0 @@
-# Desenvolvido por Daniel Schinaider de Oliveira
-
-# 3 - Nome na vertical. Faça um programa que solicite o nome do usuário e imprima-o na vertical.
-# Entrada de dado para variavel string
-nome = str(input("Informe seu nome: "))
-
-# loop para escrever o caracter de cada posicao da string
-for x in range(len(nome)):
-    print(nome[x])
