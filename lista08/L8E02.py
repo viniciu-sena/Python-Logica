fa86f0351b89eaef0ea28354364a67791fc1e739
Kelvin Schneider <54813386+KelvinSchneider@users.noreply.github.com>
2021-02-11 14:41:36 -0300

    Create L8E02.py

---
+++ lista08/L8E02.py
@@ -0,0 +1,18 @@
+# Feito por Kelvin Schneider
+#02
+
+'''Como no exercicio não estava especificado, deixei um valor pré definido
+e dentro do método é aumentado o tomando do lado em 10%'''
+
+class Quadrado:
+  def __init__(self, tLado):
+    self.tLado = tLado
+
+  def mudarValor(self, tLado):
+    self.tLado = tLado * 1.1
+    print("Area :", self.tLado)
+
+  tLado = 20
+
+classQuadrado = Quadrado(tLado)
+classQuadrado.mudarValor(tLado)
